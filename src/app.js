8f0e6781dc86b3818588921887a7fa4c2a9608dd
sebastian <sebastianseban0105@gmail.com>
2026-02-04 10:15:03 +0530

    first commit

---
+++ src/app.js
@@ -0,0 +1,33 @@
+import compression from "compression";
+import cookieParser from "cookie-parser";
+import express from "express";
+import helmet from "helmet";
+import cors from "cors";
+
+import { errorHandler, notFound } from "./middleware/errorHandler.middleware.js";
+
+const app = express()
+
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(compression());
+app.use(helmet());
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
+
+
+app.get("/health", (req, res) => {
+  res.send("lumilogic Backend Running 🚀");
+});
+
+
+app.use(notFound);
+app.use(errorHandler);
+
+export {app}
